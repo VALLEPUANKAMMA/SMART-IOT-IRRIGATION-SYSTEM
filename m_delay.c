61377d8889d224311729f4e9aa2529eb6e5b023d
VALLEPUANKAMMA <vallepuankamma88@gmail.com>
2026-05-26 14:30:27 +0530

    Update m_delay.c

--- m_delay.c
+++ m_delay.c
@@ -1,13 +1,23 @@
+/* Include user-defined typedef header file */
 #include "typedef.h"
+
+/* Function to generate microsecond delay */
 void delay_us(u32 dlys)
 {
-for(dlys*=12;dlys>0;dlys--);
+	/* Loop for required microsecond delay */
+	for(dlys*=12;dlys>0;dlys--);
 }
+
+/* Function to generate millisecond delay */
 void delay_ms(u32 dlys)
 {
-for(dlys*=12000;dlys>0;dlys--);
+	/* Loop for required millisecond delay */
+	for(dlys*=12000;dlys>0;dlys--);
 }
+
+/* Function to generate second delay */
 void delay_s(u32 dlys)
 {
-for(dlys*=12000000;dlys>0;dlys--);
+	/* Loop for required second delay */
+	for(dlys*=12000000;dlys>0;dlys--);
 }
