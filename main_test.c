3ee0bd82d2dc9fd9b8c0cb3fafd6436e53a67694
VALLEPUANKAMMA <vallepuankamma88@gmail.com>
2026-05-26 14:41:47 +0530

    Update main_test.c

--- main_test.c
+++ main_test.c
@@ -1,154 +1,364 @@
-#include<lpc21xx.h>
+/*
+ Include LPC21xx microcontroller register definitions
+*/
+#include <lpc21xx.h>
+
+/*
+ Include LCD function declarations
+*/
 #include "lcd.h"
+
+/*
+ Include keypad module declarations
+*/
 #include "kpm.h"
+
+/*
+ Include UART0 communication functions
+*/
 #include "uart0.h"
+
+/*
+ Include ESP01 Wi-Fi module functions
+*/
 #include "esp01.h"
+
+/*
+ RTC header is currently not used
+*/
 //#include "rtc.h"
+
+/*
+ Include delay functions
+*/
 #include "delay.h"
+
+/*
+ Include external interrupt functions
+*/
 #include "m_int0.h"
+
+/*
+ Include UART0 again (duplicate include)
+*/
 #include "uart0.h"
+
+/*
+ Include DHT11 sensor functions
+*/
 #include "dht11.h"
 
+/*
+ Include main DHT11 related declarations
+*/
 #include "main_dht11.h"
+
+/*
+ Include main project function declarations
+*/
 #include "main_code.h"
 
+
+/*
+ Define ThingSpeak send interval
+ Data will be sent every 3 minutes
+*/
 #define SEND_INTERVAL 3
+
+
+/*
+ External interrupt flag variable
+*/
 extern int flag;
 
- int humidity,temperature;
 
-int last_sent_min=-1;
+/*
+ Global variables to store final humidity
+ and temperature values
+*/
+int humidity, temperature;
 
+
+/*
+ Stores the last minute at which
+ data was sent to ThingSpeak
+*/
+int last_sent_min = -1;
+
+
+
+/*
+ Main function starts here
+*/
 int main()
 {
-	unsigned char humidity_integer, humidity_decimal, temp_integer, temp_decimal, checksum;
-   
-   
+	/*
+	 Variables to store DHT11 raw data
+	*/
+	unsigned char humidity_integer;
+	unsigned char humidity_decimal;
+	unsigned char temp_integer;
+	unsigned char temp_decimal;
+	unsigned char checksum;
 
-	IODIR0=1<<20;//FOR MOTOR TURN ON
-	
-	IODIR0&=~(1<<21);//FOR READING THE SOIL MOSITURE SENSOR 0 WET 1 DRY
-	
+	/*
+	 Configure P0.20 as output
+	 Used to control motor ON/OFF
+	*/
+	IODIR0 = 1 << 20;
 
-	
-    InitUART0();
-	
+	/*
+	 Configure P0.21 as input
+	 Used to read soil moisture sensor
+	 0 = Wet
+	 1 = Dry
+	*/
+	IODIR0 &= ~(1 << 21);
+
+	/*
+	 Initialize UART0 communication
+	*/
+	InitUART0();
+
+	/*
+	 Initialize LCD display
+	*/
 	LCD_Init();
 
-	Write_str_LCD("SMART IRRIGATION");	
-	  delay_ms(1000);
-    Write_CMD_LCD(0x01);
+	/*
+	 Display project title
+	*/
+	Write_str_LCD("SMART IRRIGATION");
 
-   Initkpm();
-	
-	init_int();
-	
-	rtc_init();
-	
-	esp01_connectAP();
-	
+	/*
+	 Wait 1 second
+	*/
 	delay_ms(1000);
 
+	/*
+	 Clear LCD
+	*/
 	Write_CMD_LCD(0x01);
-		//Write_str_LCD("ok");
-		//delay_ms(1000);
+
+	/*
+	 Initialize keypad module
+	*/
+	Initkpm();
+
+	/*
+	 Initialize external interrupt
+	*/
+	init_int();
+
+	/*
+	 Initialize Real Time Clock
+	*/
+	rtc_init();
+
+	/*
+	 Connect ESP01 to Wi-Fi
+	*/
+	esp01_connectAP();
+
+	/*
+	 Wait for stability
+	*/
+	delay_ms(1000);
+
+	/*
+	 Clear LCD
+	*/
+	Write_CMD_LCD(0x01);
+
+	/*
+	 Infinite loop starts
+	*/
 	while(1)
 	{
-	
-
-	//	dht11_display();
-
+		/*
+		 Delay 3 seconds before
+		 reading DHT11 sensor
+		*/
 		delay_s(3);
 
-		//Write_str_LCD("dht11");	
-		
+		/*
+		 Send start signal to DHT11
+		*/
 		dht11_request();
 
-
-
+		/*
+		 Wait for DHT11 response
+		*/
 		dht11_response();
 
+		/*
+		 Read humidity integer part
+		*/
 		humidity_integer = dht11_data();
 
+		/*
+		 Read humidity decimal part
+		*/
 		humidity_decimal = dht11_data();
 
+		/*
+		 Read temperature integer part
+		*/
 		temp_integer = dht11_data();
 
+		/*
+		 Read temperature decimal part
+		*/
 		temp_decimal = dht11_data();
 
-checksum = dht11_data();
+		/*
+		 Read checksum byte
+		*/
+		checksum = dht11_data();
 
-		humidity=humidity_integer+humidity_decimal;
+		/*
+		 Calculate total humidity
+		*/
+		humidity = humidity_integer + humidity_decimal;
 
-		temperature=temp_integer+temp_decimal;
+		/*
+		 Calculate total temperature
+		*/
+		temperature = temp_integer + temp_decimal;
 
-		if( (humidity_integer + humidity_decimal + temp_integer + temp_decimal) != checksum )
-		  {
+		/*
+		 Verify checksum correctness
+		*/
+		if((humidity_integer + humidity_decimal +
+		    temp_integer + temp_decimal) != checksum)
+		{
+			/*
+			 Display checksum error
+			*/
 			Write_str_LCD("Checksum Error\r\n");
-		   }
+		}
 		else
 		{
-		   	//Write_DAT_LCD('.');
-
+			/*
+			 Move LCD cursor to first line
+			*/
 			Write_CMD_LCD(0x80);
 
+			/*
+			 Display humidity label
+			*/
 			Write_str_LCD("Humidity : ");
 
+			/*
+			 Display humidity integer
+			*/
 			Write_int_LCD(humidity_integer);
 
+			/*
+			 Display decimal point
+			*/
 			Write_DAT_LCD('.');
 
+			/*
+			 Display humidity decimal
+			*/
 			Write_int_LCD(humidity_decimal);
 
+			/*
+			 Display humidity unit
+			*/
 			Write_str_LCD(" % RH ");
 
+			/*
+			 Move cursor to second line
+			*/
 			Write_CMD_LCD(0xC0);
 
+			/*
+			 Display temperature label
+			*/
 			Write_str_LCD("Temprature : ");
 
+			/*
+			 Display temperature integer
+			*/
 			Write_int_LCD(temp_integer);
 
+			/*
+			 Display decimal point
+			*/
 			Write_DAT_LCD('.');
 
+			/*
+			 Display temperature decimal
+			*/
 			Write_int_LCD(temp_decimal);
 
+			/*
+			 Display degree symbol
+			*/
 			Write_DAT_LCD(223);
 
+			/*
+			 Display Celsius symbol
+			*/
 			Write_str_LCD("C");
 
+			/*
+			 Move cursor to fourth line
+			*/
 			Write_CMD_LCD(0xD4);
 
+			/*
+			 Display checksum label
+			*/
 			Write_str_LCD("Checksum : ");
 
+			/*
+			 Display checksum value
+			*/
 			Write_int_LCD(checksum);
 
+			/*
+			 Delay 3 seconds so user can read
+			*/
 			delay_ms(3000);
 
-			//Write_CMD_LCD(0x01);
-			//arr[0]=humidity_integer;
+			/*
+			 Check if current minute is a
+			 multiple of SEND_INTERVAL
+			 and ensure data is sent only once
+			*/
+			if((MIN % SEND_INTERVAL == 0) &&
+			   (MIN != last_sent_min))
+			{
+				/*
+				 Send humidity and temperature
+				 to ThingSpeak cloud
+				*/
+				esp01_sendToThingspeak1(
+					humidity,
+					temperature
+				);
 
-			//arr[1]=temp_integer;
-
-			 if((MIN%SEND_INTERVAL==0) && (MIN!=last_sent_min))
-			 {
-					esp01_sendToThingspeak1(humidity,temperature);
-					last_sent_min=MIN;
-			 }
-			
-			//delay_s(16);
-
-			//esp01_sendToThingspeak2(temperature);
-			
-			
+				/*
+				 Update last sent minute
+				*/
+				last_sent_min = MIN;
 			}
+		}
 
-		 	
+		/*
+		 Check if interrupt occurred
+		 and open configuration menu
+		*/
+		menu_base_int();
 
-		 menu_base_int();
-	
-	compare_temp_hum();
-
-		//delay_s(16);
-			
+		/*
+		 Compare current temperature
+		 and humidity with user thresholds
+		 and control motor
+		*/
+		compare_temp_hum();
 	}
 }
