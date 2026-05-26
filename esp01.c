04b6fbad230a200946bf926be0c8244805b6be6e
VALLEPUANKAMMA <vallepuankamma88@gmail.com>
2026-05-26 14:24:32 +0530

    Update esp01.c

--- esp01.c
+++ esp01.c
@@ -1,647 +1,776 @@
+/* Include string handling functions */
 #include <string.h>
 
+/* Include UART0 header file */
 #include "uart0.h"
 
+/* Include delay function header */
 #include "delay.h"
 
+/* Include LCD function header */
 #include "lcd.h"
 
-
-
-
+/* External buffer to store ESP01 response */
 extern char buff[200];
 
+/* External variable to track buffer index */
 extern unsigned char i;
 
-
+/* Function to connect ESP01 to Wi-Fi access point */
 void esp01_connectAP()
 {
-
+	/* Clear LCD display */
 	Write_CMD_LCD(0x01);
 
+	/* Move cursor to first row */
 	Write_CMD_LCD(0x80);
 
+	/* Display "AT" on LCD */
 	Write_str_LCD("AT");
 
+	/* Wait for 1 second */
 	delay_ms(1000);
 
+	/* Send AT command to ESP01 */
 	UART0_Str("AT\r\n");
 
+	/* Reset index and clear receive buffer */
 	i=0;memset(buff,'\0',200);
 
+	/* Wait until 4 characters are received */
 	while(i<4);
 
+	/* Delay for response stability */
 	delay_ms(500);
 
+	/* Add null terminator to received string */
 	buff[i] = '\0';
 
+	/* Clear LCD display */
 	Write_CMD_LCD(0x01);
 
+	/* Move cursor to first row */
 	Write_CMD_LCD(0x80);
 
+	/* Display received response */
 	Write_str_LCD(buff);
 
+	/* Delay for 2 seconds */
 	delay_ms(2000);
 
+	/* Check if response contains "OK" */
 	if(strstr(buff,"OK"))
 	{
-
+		/* Move cursor to second row */
 		Write_CMD_LCD(0xC0);
 
+		/* Display "OK" */
 		Write_str_LCD("OK");
 
+		/* Delay for 1 second */
 		delay_ms(1000);		
-
 	}
-
 	else
 	{
-
+		/* Move cursor to second row */
 		Write_CMD_LCD(0xC0);
 
+		/* Display "ERROR" */
 		Write_str_LCD("ERROR");
 
+		/* Delay for 1 second */
 		delay_ms(1000);		
 
+		/* Exit function if error */
 		return;
-
 	}
 
-	
-
-	
-
+	/* Clear LCD display */
 	Write_CMD_LCD(0x01);
 
+	/* Move cursor to first row */
 	Write_CMD_LCD(0x80);
 
+	/* Display "ATE0" on LCD */
 	Write_str_LCD("ATE0");
 
+	/* Delay for 1 second */
 	delay_ms(1000);
 
+	/* Send ATE0 command to disable echo */
 	UART0_Str("ATE0\r\n");
 
+	/* Reset index and clear buffer */
 	i=0;memset(buff,'\0',200);
 
+	/* Wait until 4 characters received */
 	while(i<4);
 
+	/* Delay for response stability */
 	delay_ms(500);
 
+	/* Null terminate received string */
 	buff[i] = '\0';
 
+	/* Clear LCD display */
 	Write_CMD_LCD(0x01);
 
+	/* Move cursor to first row */
 	Write_CMD_LCD(0x80);
 
+	/* Display received response */
 	Write_str_LCD(buff);
 
+	/* Delay for 2 seconds */
 	delay_ms(2000);
 
+	/* Check if response contains "OK" */
 	if(strstr(buff,"OK"))
 	{
-
+		/* Move cursor to second row */
 		Write_CMD_LCD(0xC0);
 
+		/* Display "OK" */
 		Write_str_LCD("OK");
 
+		/* Delay for 1 second */
 		delay_ms(1000);		
-
 	}
-
 	else
 	{
-
+		/* Move cursor to second row */
 		Write_CMD_LCD(0xC0);
 
+		/* Display "ERROR" */
 		Write_str_LCD("ERROR");
 
+		/* Delay for 1 second */
 		delay_ms(1000);		
 
+		/* Exit function */
 		return;
-
 	}
 
-	
-
-	
-
+	/* Clear LCD display */
 	Write_CMD_LCD(0x01);
 
+	/* Move cursor to first row */
 	Write_CMD_LCD(0x80);
 
+	/* Display "AT+CIPMUX" */
 	Write_str_LCD("AT+CIPMUX");
 
+	/* Delay for 1 second */
 	delay_ms(1000);
 
+	/* Send command to set single connection mode */
 	UART0_Str("AT+CIPMUX=0\r\n");
 
+	/* Reset index and clear buffer */
 	i=0;memset(buff,'\0',200);
 
+	/* Wait for response */
 	while(i<4);
 
+	/* Delay for stability */
 	delay_ms(500);
 
+	/* Null terminate response */
 	buff[i] = '\0';
 
+	/* Clear LCD */
 	Write_CMD_LCD(0x01);
 
+	/* Move cursor */
 	Write_CMD_LCD(0x80);
 
+	/* Display response */
 	Write_str_LCD(buff);
 
+	/* Delay for 2 seconds */
 	delay_ms(2000);
 
+	/* Check for successful response */
 	if(strstr(buff,"OK"))
 	{
-
+		/* Move cursor to second row */
 		Write_CMD_LCD(0xC0);
 
+		/* Display "OK" */
 		Write_str_LCD("OK");
 
+		/* Delay */
 		delay_ms(1000);		
-
 	}
-
 	else
 	{
-
+		/* Move cursor to second row */
 		Write_CMD_LCD(0xC0);
 
+		/* Display "ERROR" */
 		Write_str_LCD("ERROR");
 
+		/* Delay */
 		delay_ms(1000);		
 
+		/* Exit */
 		return;
-
 	}
-
-	
-
+	/* Clear LCD display */
 	Write_CMD_LCD(0x01);
 
+	/* Move cursor to first row */
 	Write_CMD_LCD(0x80);
 
+	/* Display "AT+CWQAP" on LCD */
 	Write_str_LCD("AT+CWQAP");
 
+	/* Wait for 1 second */
 	delay_ms(1000);
 
+	/* Send command to disconnect from current Wi-Fi */
 	UART0_Str("AT+CWQAP\r\n");
 
+	/* Reset index and clear buffer */
 	i=0;memset(buff,'\0',200);
 
+	/* Wait until 4 characters are received */
 	while(i<4);
 
+	/* Wait for response completion */
 	delay_ms(1500);
 
+	/* Null terminate response string */
 	buff[i] = '\0';
 
+	/* Clear LCD display */
 	Write_CMD_LCD(0x01);
 
+	/* Move cursor to first row */
 	Write_CMD_LCD(0x80);
 
+	/* Display ESP01 response */
 	Write_str_LCD(buff);
 
+	/* Delay for 2 seconds */
 	delay_ms(2000);
 
+	/* Check if response contains "OK" */
 	if(strstr(buff,"OK"))
 	{
-
+		/* Move cursor to second row */
 		Write_CMD_LCD(0xC0);
 
+		/* Display success message */
 		Write_str_LCD("OK");
 
+		/* Delay for 1 second */
 		delay_ms(1000);		
-
 	}
 	else
 	{
-
+		/* Move cursor to second row */
 		Write_CMD_LCD(0xC0);
 
+		/* Display error message */
 		Write_str_LCD("ERROR");
 
+		/* Delay for 1 second */
 		delay_ms(1000);		
 
+		/* Exit function */
 		return;
-
 	}
 
-	
-
-	
-
-	
-
+	/* Clear LCD display */
 	Write_CMD_LCD(0x01);
 
+	/* Move cursor to first row */
 	Write_CMD_LCD(0x80);
 
+	/* Display "AT+CWJAP" */
 	Write_str_LCD("AT+CWJAP");
 
+	/* Wait for 1 second */
 	delay_ms(1000);
 
+	/* Need to change Wi-Fi name and password */
 	//need to change the wifi network name and password
 
+	/* Send Wi-Fi connection command with SSID and password */
 	UART0_Str("AT+CWJAP=\"AK\",\"123456789\"\r\n");
 
+	/* Reset index and clear buffer */
 	i=0;memset(buff,'\0',200);
 
+	/* Wait until 4 characters received */
 	while(i<4);
 
+	/* Wait longer for Wi-Fi connection */
 	delay_ms(2500);
 
+	/* Null terminate received string */
 	buff[i] = '\0';
 
+	/* Clear LCD */
 	Write_CMD_LCD(0x01);
 
+	/* Move cursor to first row */
 	Write_CMD_LCD(0x80);
 
+	/* Display received response */
 	Write_str_LCD(buff);
 
+	/* Delay for 2 seconds */
 	delay_ms(2000);
 
+	/* Check if Wi-Fi connected successfully */
 	if(strstr(buff,"WIFI CONNECTED"))
 	{
-
+		/* Move cursor to second row */
 		Write_CMD_LCD(0xC0);
 
+		/* Display "OK" */
 		Write_str_LCD("OK");
 
+		/* Delay for 1 second */
 		delay_ms(1000);		
-
 	}
-
 	else
 	{
-
+		/* Move cursor to second row */
 		Write_CMD_LCD(0xC0);
 
+		/* Display "ERROR" */
 		Write_str_LCD("ERROR");
 
+		/* Delay for 1 second */
 		delay_ms(1000);		
 
+		/* Exit function */
 		return;
-
 	}
-
-	
-
 }
 
-
+/* Function to send temperature and humidity data to ThingSpeak */
 void esp01_sendToThingspeak1(float val1,float val2)
 {
-
+	/* Commented LCD clear command */
 	//Write_CMD_LCD(0x01);
 
-//	Write_CMD_LCD(0x80);
+	/* Commented LCD cursor set */
+	//	Write_CMD_LCD(0x80);
 
+	/* Commented LCD display command */
 	//Write_str_LCD("AT+CIPSTART");
 
+	/* Wait for 1 second */
 	delay_ms(1000);
 
+	/* Start TCP connection to ThingSpeak server */
 	UART0_Str("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80\r\n");
 
+	/* Reset index and clear buffer */
 	i=0;memset(buff,'\0',200);
 
+	/* Wait until 5 characters received */
 	while(i<5);
 
+	/* Wait for connection response */
 	delay_ms(2500);
 
+	/* Null terminate response */
 	buff[i] = '\0';
 
-//	Write_CMD_LCD(0x01);
-
-//	Write_CMD_LCD(0x80);
-
-//	Write_str_LCD(buff);
-
+	/* Delay for 2 seconds */
 	delay_ms(2000);
 
+	/* Check if connection successful */
 	if(strstr(buff,"CONNECT") || strstr(buff,"ALREADY CONNECTED"))
 	{
-
-//		Write_CMD_LCD(0xC0);
-
-//		Write_str_LCD("OK");
-
+		/* Delay before next command */
 		delay_ms(1000);
 
-		
-
-//		Write_CMD_LCD(0x01);
-
-//		Write_CMD_LCD(0x80);
-
-//		Write_str_LCD("AT+CIPSEND");
-
+		/* Delay before sending data */
 		delay_ms(1000);
 
+		/* Send command specifying data length */
 		UART0_Str("AT+CIPSEND=64\r\n");
 
+		/* Reset buffer */
 		i=0;memset(buff,'\0',200);
 
+		/* Wait for prompt */
 		//while(buff[i] != '>');
 
+		/* Delay */
 		delay_ms(500);
 
+		/* Need to change ThingSpeak API key */
 		//need to change the thingspeak write API key accordind to your channel
 
+		/* Send HTTP GET request with API key */
 		UART0_Str("GET /update?api_key=HWVLM0FXWP9G7IAM&field1=");
 
+		/* Send humidity value */
 		UART0_Float(val2);
 
-		 UART0_Str("&field2=");
+		/* Send second field identifier */
+		UART0_Str("&field2=");
 
-		 UART0_Float(val1);
+		/* Send temperature value */
+		UART0_Float(val1);
 
+		/* End HTTP request */
 		UART0_Str("\r\n\r\n");
 
+		/* Wait for upload */
 		delay_ms(5000);
 
+		/* Additional delay */
 		delay_ms(5000);
 
+		/* Null terminate buffer */
 		buff[i] = '\0';
 
-//		Write_CMD_LCD(0x01);
-
-//		Write_CMD_LCD(0x80);
-
-//		Write_str_LCD(buff);
-
+		/* Delay for response */
 		delay_ms(2000);
 
+		/* Check if data sent successfully */
 		if(strstr(buff,"SEND OK"))
-
 		{
-
-			//Write_CMD_LCD(0x01);
-
-			//Write_str_LCD("DATA UPDATED");
-
+			/* Delay after successful update */
 			delay_ms(1000);			
-
-			//Write_CMD_LCD(0x01);
 		}
-
 		else
-
 		{
-
+			/* Clear LCD */
 			Write_CMD_LCD(0x01);
 
+			/* Show error message */
 			Write_str_LCD("TEMPRATURE IS NOT UPDATED");
 
+			/* Delay */
 			delay_ms(1000);
 			
+			/* Clear LCD */
 			Write_CMD_LCD(0x01);	
-
 		}
-
-		
-
 	}
-	
 	else
-
 	{
-
+		/* Move cursor to second row */
 		Write_CMD_LCD(0xC0);
 
+		/* Display connection error */
 		Write_str_LCD("ERROR1");
 
+		/* Delay */
 		delay_ms(1000);
 		
+		/* Clear LCD */
 		Write_CMD_LCD(0x01);		
 
+		/* Exit function */
 		return;
-
 	}
-
 }
-
+/* Function to send humidity data to ThingSpeak */
 void esp01_sendToThingspeak2(float val)
-
 {
+	/* Commented LCD clear command */
+	//	Write_CMD_LCD(0x01);
 
-//	Write_CMD_LCD(0x01);
+	/* Commented LCD cursor command */
+	//	Write_CMD_LCD(0x80);
 
-//	Write_CMD_LCD(0x80);
+	/* Commented LCD display text */
+	//	Write_str_LCD("AT+CIPSTART");
 
-//	Write_str_LCD("AT+CIPSTART");
-
+	/* Wait for 1 second */
 	delay_ms(1000);
 
+	/* Start TCP connection with ThingSpeak server */
 	UART0_Str("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80\r\n");
 
-	i=0;memset(buff,'\0',200);
+	/* Reset buffer index and clear buffer */
+	i=0;
+	memset(buff,'\0',200);
 
+	/* Wait until at least 5 characters received */
 	while(i<5);
 
+	/* Wait for response completion */
 	delay_ms(2500);
 
+	/* Add string terminator */
 	buff[i] = '\0';
 
-//	Write_CMD_LCD(0x01);
+	/* Commented LCD commands */
+	//	Write_CMD_LCD(0x01);
+	//	Write_CMD_LCD(0x80);
+	//	Write_str_LCD(buff);
 
-//	Write_CMD_LCD(0x80);
-
-//	Write_str_LCD(buff);
-
+	/* Delay for 2 seconds */
 	delay_ms(2000);
 
+	/* Check if ESP connected successfully */
 	if(strstr(buff,"CONNECT") || strstr(buff,"ALREADY CONNECTED"))
 	{
+		/* Commented LCD success display */
+		//	Write_CMD_LCD(0xC0);
+		//	Write_str_LCD("OK");
 
-//		Write_CMD_LCD(0xC0);
-
-//		Write_str_LCD("OK");
-
+		/* Wait before sending data */
 		delay_ms(1000);
 
-		
+		/* Commented LCD commands */
+		//	Write_CMD_LCD(0x01);
+		//	Write_CMD_LCD(0x80);
+		//	Write_str_LCD("AT+CIPSEND");
 
-	//	Write_CMD_LCD(0x01);
-  
-//		Write_CMD_LCD(0x80);
-
-//		Write_str_LCD("AT+CIPSEND");
-
+		/* Wait for 1 second */
 		delay_ms(1000);
 
+		/* Send data length command */
 		UART0_Str("AT+CIPSEND=51\r\n");
 
-		i=0;memset(buff,'\0',200);
+		/* Reset index and clear buffer */
+		i=0;
+		memset(buff,'\0',200);
 
+		/* Wait for > prompt (commented) */
 		//while(buff[i] != '>');
 
+		/* Delay before sending GET request */
 		delay_ms(500);
 
+		/* Need to change ThingSpeak API key */
 		//need to change the thingspeak write API key accordind to your channel
 
-		UART0_Str("GET /update?api_key=HWVLM0FXWP9G7IAM&field2=");		   //"GET /update?api_key=A4IW8PQR3L51TS14&field1="
+		/* Send HTTP GET request with field2 */
+		UART0_Str("GET /update?api_key=HWVLM0FXWP9G7IAM&field2=");
 
-	//	UART0_Str(val);
+		/* Commented string send */
+		//	UART0_Str(val);
 
+		/* Send float humidity value */
 		UART0_Float(val);
 
+		/* End HTTP request */
 		UART0_Str("\r\n\r\n");
 
+		/* Wait for upload */
 		delay_ms(5000);
 
+		/* Additional wait */
 		delay_ms(5000);
 
+		/* Null terminate buffer */
 		buff[i] = '\0';
 
-//		Write_CMD_LCD(0x01);
+		/* Commented LCD response display */
+		//	Write_CMD_LCD(0x01);
+		//	Write_CMD_LCD(0x80);
+		//	Write_str_LCD(buff);
 
-//		Write_CMD_LCD(0x80);
-
-//		Write_str_LCD(buff);
-
+		/* Delay for 2 seconds */
 		delay_ms(2000);
 
+		/* Check if data was sent successfully */
 		if(strstr(buff,"SEND OK"))
-
 		{
+			/* Commented LCD success message */
+			//	Write_CMD_LCD(0x01);
+			//	Write_str_LCD("DATA UPDATED");
 
-//			Write_CMD_LCD(0x01);
+			/* Delay after successful transmission */
+			delay_ms(1000);
 
-		//	Write_str_LCD("DATA UPDATED");
-
-			delay_ms(1000);			
-
-//			Write_CMD_LCD(0x01);
+			/* Commented LCD clear */
+			//	Write_CMD_LCD(0x01);
 		}
-
 		else
-
 		{
-
+			/* Clear LCD */
 			Write_CMD_LCD(0x01);
 
+			/* Show humidity update failure */
 			Write_str_LCD("HUMIDITY IS NOT UPADTED");
 
+			/* Delay for 1 second */
 			delay_ms(1000);
-			
-			Write_CMD_LCD(0x01);	
 
+			/* Clear LCD */
+			Write_CMD_LCD(0x01);
 		}
-
-		
-
 	}
-
 	else
-
 	{
-
+		/* Move cursor to second row */
 		Write_CMD_LCD(0xC0);
 
+		/* Display connection error */
 		Write_str_LCD("ERROR2");
 
+		/* Delay for 1 second */
 		delay_ms(1000);
-				
+
+		/* Clear LCD */
 		Write_CMD_LCD(0x01);
 
+		/* Exit function */
 		return;
-
 	}
+}
 
 
-
-}  
-
+/* Function to send motor status data to ThingSpeak */
 void esp01_sendToThingspeak3(float val)
 {
-    // Write_CMD_LCD(0x01);
-    // Write_CMD_LCD(0x80);
-    // Write_str_LCD("AT+CIPSTART");
+	/* Commented LCD clear */
+	// Write_CMD_LCD(0x01);
 
-    delay_ms(1000);
+	/* Commented cursor set */
+	// Write_CMD_LCD(0x80);
 
-    UART0_Str("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80\r\n");
+	/* Commented LCD text */
+	// Write_str_LCD("AT+CIPSTART");
 
-    i = 0;
-    memset(buff, '\0', 200);
+	/* Wait for 1 second */
+	delay_ms(1000);
 
-    while(i < 5);
+	/* Start TCP connection to ThingSpeak */
+	UART0_Str("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80\r\n");
 
-    delay_ms(2500);
+	/* Reset index */
+	i = 0;
 
-    buff[i] = '\0';
+	/* Clear receive buffer */
+	memset(buff, '\0', 200);
 
-    // Write_CMD_LCD(0x01);
-    // Write_CMD_LCD(0x80);
-    // Write_str_LCD(buff);
+	/* Wait until response received */
+	while(i < 5);
 
-    delay_ms(2000);
+	/* Wait for connection completion */
+	delay_ms(2500);
 
-    if(strstr(buff, "CONNECT") || strstr(buff, "ALREADY CONNECTED"))
-    {
-        // Write_CMD_LCD(0xC0);
-        // Write_str_LCD("OK");
+	/* Add null terminator */
+	buff[i] = '\0';
 
-        delay_ms(1000);
+	/* Commented LCD display */
+	// Write_CMD_LCD(0x01);
+	// Write_CMD_LCD(0x80);
+	// Write_str_LCD(buff);
 
-        // Write_CMD_LCD(0x01);
-        // Write_CMD_LCD(0x80);
-        // Write_str_LCD("AT+CIPSEND");
+	/* Delay for 2 seconds */
+	delay_ms(2000);
 
-        delay_ms(1000);
+	/* Check connection success */
+	if(strstr(buff, "CONNECT") || strstr(buff, "ALREADY CONNECTED"))
+	{
+		/* Commented LCD OK display */
+		// Write_CMD_LCD(0xC0);
+		// Write_str_LCD("OK");
 
-        UART0_Str("AT+CIPSEND=51\r\n");
+		/* Wait before next command */
+		delay_ms(1000);
 
-        i = 0;
-        memset(buff, '\0', 200);
+		/* Commented LCD commands */
+		// Write_CMD_LCD(0x01);
+		// Write_CMD_LCD(0x80);
+		// Write_str_LCD("AT+CIPSEND");
 
-        // while(buff[i] != '>');
+		/* Wait 1 second */
+		delay_ms(1000);
 
-        delay_ms(500);
+		/* Send data length command */
+		UART0_Str("AT+CIPSEND=51\r\n");
 
-        // Change the ThingSpeak Write API key according to your channel
-        UART0_Str("GET /update?api_key=HWVLM0FXWP9G7IAM&field3=");
+		/* Reset index */
+		i = 0;
 
-        UART0_Float(val);
+		/* Clear buffer */
+		memset(buff, '\0', 200);
 
-        UART0_Str("\r\n\r\n");
+		/* Wait for prompt (commented) */
+		// while(buff[i] != '>');
 
-        delay_ms(5000);
-        delay_ms(5000);
+		/* Delay before GET request */
+		delay_ms(500);
 
-        buff[i] = '\0';
+		/* Change API key according to your ThingSpeak channel */
+		// Change the ThingSpeak Write API key according to your channel
 
-        // Write_CMD_LCD(0x01);
-        // Write_CMD_LCD(0x80);
-        // Write_str_LCD(buff);
+		/* Send GET request with field3 */
+		UART0_Str("GET /update?api_key=HWVLM0FXWP9G7IAM&field3=");
 
-        delay_ms(2000);
+		/* Send motor status float value */
+		UART0_Float(val);
 
-        if(strstr(buff, "SEND OK"))
-        {
-           // Write_CMD_LCD(0x01);
-            //Write_str_LCD("DATA UPDATED");
-            delay_ms(1000);
+		/* End HTTP request */
+		UART0_Str("\r\n\r\n");
 
-            //Write_CMD_LCD(0x01);
-        }
-        else
-        {
-            Write_CMD_LCD(0x01);
-            Write_str_LCD("MOTOR DATA NOT UPDATED");
-            delay_ms(1000);
+		/* Wait for transmission */
+		delay_ms(5000);
 
-            Write_CMD_LCD(0x01);
-        }
-    }
-    else
-    {
-        Write_CMD_LCD(0xC0);
-        Write_str_LCD("ERROR2");
-        delay_ms(1000);
+		/* Additional wait */
+		delay_ms(5000);
 
-        Write_CMD_LCD(0x01);
+		/* Null terminate response */
+		buff[i] = '\0';
 
-        return;
-    }
+		/* Commented LCD display */
+		// Write_CMD_LCD(0x01);
+		// Write_CMD_LCD(0x80);
+		// Write_str_LCD(buff);
+
+		/* Delay for response */
+		delay_ms(2000);
+
+		/* Check if data sent successfully */
+		if(strstr(buff, "SEND OK"))
+		{
+			/* Commented success message */
+			// Write_CMD_LCD(0x01);
+			// Write_str_LCD("DATA UPDATED");
+
+			/* Delay */
+			delay_ms(1000);
+
+			/* Commented LCD clear */
+			// Write_CMD_LCD(0x01);
+		}
+		else
+		{
+			/* Clear LCD */
+			Write_CMD_LCD(0x01);
+
+			/* Display motor update failure */
+			Write_str_LCD("MOTOR DATA NOT UPDATED");
+
+			/* Delay */
+			delay_ms(1000);
+
+			/* Clear LCD */
+			Write_CMD_LCD(0x01);
+		}
+	}
+	else
+	{
+		/* Move cursor to second line */
+		Write_CMD_LCD(0xC0);
+
+		/* Display error message */
+		Write_str_LCD("ERROR2");
+
+		/* Delay */
+		delay_ms(1000);
+
+		/* Clear LCD */
+		Write_CMD_LCD(0x01);
+
+		/* Exit function */
+		return;
+	}
 }
