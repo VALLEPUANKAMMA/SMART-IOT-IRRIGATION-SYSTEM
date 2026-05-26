463ef19e976d96594431943c18ff841d9f3a41e0
VALLEPUANKAMMA <vallepuankamma88@gmail.com>
2026-05-26 14:45:01 +0530

    Update typedef.h

--- typedef.h
+++ typedef.h
@@ -1,6 +1,52 @@
-typedef unsigned int u32; 
+/*
+ Type definition for 32-bit unsigned integer
+
+ u32 is used to represent unsigned values
+ ranging from 0 to 4,294,967,295
+*/
+typedef unsigned int u32;
+
+
+/*
+ Type definition for 32-bit signed integer
+
+ s32 is used to represent signed values
+ ranging from -2,147,483,648 to 2,147,483,647
+*/
 typedef signed int s32;
+
+
+/*
+ Type definition for 8-bit unsigned character
+
+ u8 is used for byte-level data (0 to 255)
+ commonly used for sensor data, UART, etc.
+*/
 typedef unsigned char u8;
+
+
+/*
+ Type definition for 8-bit signed character
+
+ s8 is used for signed byte values
+ ranging from -128 to 127
+*/
 typedef signed char s8;
+
+
+/*
+ Type definition for 16-bit unsigned integer
+
+ u16 is used for medium range unsigned values
+ ranging from 0 to 65,535
+*/
 typedef unsigned short int u16;
+
+
+/*
+ Type definition for 16-bit signed integer
+
+ s16 is used for signed 16-bit values
+ ranging from -32,768 to 32,767
+*/
 typedef signed short int s16;
