6b3c30d2463dc574a960256513c3306623295b7b
VALLEPUANKAMMA <vallepuankamma88@gmail.com>
2026-05-26 14:12:54 +0530

    Update delay.h

--- delay.h
+++ delay.h
@@ -1,4 +1,14 @@
+/* Include user-defined typedef header file */
 #include "typedef.h"
+
+/* Function declaration for microsecond delay
+   dlys specifies delay duration in microseconds */
 void delay_us(u32 dlys);
+
+/* Function declaration for millisecond delay
+   dlys specifies delay duration in milliseconds */
 void delay_ms(u32 dlys);
+
+/* Function declaration for second delay
+   dlys specifies delay duration in seconds */
 void delay_s(u32 dlys);
