b89f475fe6a459c2d73839605b1097881cc0d026
VALLEPUANKAMMA <vallepuankamma88@gmail.com>
2026-05-26 14:34:22 +0530

    Update m_kpm_defines.h

--- m_kpm_defines.h
+++ m_kpm_defines.h
@@ -1,9 +1,23 @@
-#define ROW0 16 //p1.16
+/* Define keypad Row 0 connected to P1.16 */
+#define ROW0 16
+
+/* Define keypad Row 1 connected to P1.17 */
 #define ROW1 17
+
+/* Define keypad Row 2 connected to P1.18 */
 #define ROW2 18
+
+/* Define keypad Row 3 connected to P1.19 */
 #define ROW3 19
 
+/* Define keypad Column 0 connected to P1.20 */
 #define COL0 20
+
+/* Define keypad Column 1 connected to P1.21 */
 #define COL1 21
+
+/* Define keypad Column 2 connected to P1.22 */
 #define COL2 22
-#define COL3 23 //p1.23
+
+/* Define keypad Column 3 connected to P1.23 */
+#define COL3 23
